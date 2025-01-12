650a9c8c5a0b3d1ee0754c32db5078757dca3845
Troy DeSpain <despaintroy@gmail.com>
2025-01-12 15:14:20 -0700

    pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,27 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
+        <link rel="manifest" href="manifest.json" />
         <meta name="theme-color" content="#292929" />
+        <title>Flight Tracker</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </head>
 
       <body>

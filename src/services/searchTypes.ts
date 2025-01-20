c994bc9102e6a8ee52d03fbe6273d7961a94d166
Troy DeSpain <despaintroy@gmail.com>
2025-01-19 19:55:03 -0700

    improve search

--- src/services/searchTypes.ts
+++ src/services/searchTypes.ts
@@ -14,34 +14,44 @@ const aircraftTypeSchema = z.object({
 
 export type AircraftType = z.infer<typeof aircraftTypeSchema>
 
+function normalizeValue(value: string): string {
+  return value.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+}
+
 export async function searchTypes(
   query: string,
   limit: number = 10
 ): Promise<AircraftType[]> {
   return new Promise((resolve, reject) => {
     const csvFilePath = path.resolve("./public/icao_list.csv")
+    const normalizedQuery = normalizeValue(query)
     const results: AircraftType[] = []
 
-    // Open the CSV file and parse it
-    fs.createReadStream(csvFilePath)
+    const stream = fs
+      .createReadStream(csvFilePath)
       .pipe(parse({columns: true, trim: true, skipEmptyLines: true}))
+
+    // Open the CSV file and parse it
+    stream
       .on("data", (row) => {
         const parsedRow = aircraftTypeSchema.safeParse(row)
+        if (!parsedRow.success) {
+          console.error("Invalid row:", row)
+          return
+        }
 
-        const normalizeValue = (value: string) =>
-          value.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+        const {aircraftType, manufacturerModel} = parsedRow.data
+        const fieldsToSearch = [
+          normalizeValue(aircraftType),
+          normalizeValue(manufacturerModel)
+        ]
 
-        const match = (() => {
-          if (!parsedRow.success) return false
+        if (fieldsToSearch.some((value) => value.includes(normalizedQuery)))
+          results.push(parsedRow.data)
 
-          return [parsedRow.data.aircraftType, parsedRow.data.manufacturerModel]
-            .map(normalizeValue)
-            .some((value) => value.includes(normalizeValue(query)))
-        })()
-
-        if (parsedRow.success && match) results.push(parsedRow.data)
+        if (results.length >= limit) stream.destroy()
       })
-      .on("end", () => resolve(results.slice(0, limit)))
+      .on("end", () => resolve(results))
       .on("error", (error) => reject(error))
   })
 }

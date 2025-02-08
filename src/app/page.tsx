6963188f496c2f0b471452672c3b6443b779a87b
Troy DeSpain <despaintroy@gmail.com>
2025-02-07 20:32:26 -0700

    aircraft type info, remove list demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 import MainMap from "@/components/MainMap/MainMap"
 
 // https://airportdb.io
+// https://www.adsbdb.com/
+// https://github.com/wiedehopf/tar1090/blob/25db2f151653a6ebcbcbb65150cedfc498eee816/html/markers.js
+// https://github.com/evilsaloon/flightstats-web-api
+// https://developers.amadeus.com/pricing
+
+// airplane icons
+// photo carousel
+// background, push notifications, badges, live activities?
+// search airports
+// Favorite track flights, search flights (Google Flights?)
+// search by other stuff, favorites
+// airport icons
+// fix location button style
+// show more airplane info from the CSV, wikipedia
+// filter in URL to share link
+// show height above selected airport
+// one tab for all search
+// POV mode
 
 export default function Home() {
   return (
     <>
       <MainMap />
-      {/*<ListDemo />*/}
       {/*<3ddemo />*/}
     </>
   )
